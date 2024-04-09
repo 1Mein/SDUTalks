25dd361c363963cac0d90bf77f79c13031327ea8
1Mein <119441676+1Mein@users.noreply.github.com>
2024-04-09 15:23:25 +0500

    comment cascade

--- app/Http/Controllers/Comment/DestroyController.php
+++ app/Http/Controllers/Comment/DestroyController.php
@@ -11,7 +11,8 @@ class DestroyController extends Controller
     public function __invoke(Comment $comment)
     {
         $comment->delete();
-        Notify::removeNotify('commented-post',$comment->user_id, $comment->post_id);
+        Notify::removeNotify('commented-post',$comment->user_id, $comment->post_id,$comment->id);
+        Notify::removeNotify('replied-comment',$comment->user_id, $comment->post_id,$comment->id);
         return response()->json(['id' => $comment->id]);
     }
 }
